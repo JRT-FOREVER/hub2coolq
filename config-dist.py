39ed404de44e8f4a1ed0a11f8d59cd313e014032
metal A-wing <shiyongxin@aliyun.com>
2018-04-02 21:42:06 +0800

    Update hub2coolq.py

--- config-dist.py
+++ config-dist.py
@@ -1,5 +1,5 @@
-
 group_id = '233'
+msg_prefix = ' Github Message '
 
 api_root='http://127.0.0.1:5700/'
 access_token='your-token'
--- hub2coolq.py
+++ hub2coolq.py
@@ -11,5 +11,6 @@ app = Flask(__name__)
 
 @app.route('/github', methods=['POST', 'GET'])
 def github():
-    return jsonify(bot.send_group_msg(group_id=config.group_id, message=config.msg_prefix+request.json["sender"]["login"]))
-
+    msg = config.msg_prefix + request.headers['X-GitHub-Event'] + ' by ' +  request.json["sender"]["login"]
+    #return jsonify(msg)
+    return jsonify(bot.send_group_msg(group_id=config.group_id, message=msg))
